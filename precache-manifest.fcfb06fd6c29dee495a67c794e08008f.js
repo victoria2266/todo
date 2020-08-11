d9ee799447995079d30611a1576ab109fce56d59
Victoria <testversion2617@gmx.de>
2020-08-11 14:17:16 +0200

    Updates

---
+++ precache-manifest.fcfb06fd6c29dee495a67c794e08008f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "631f0314210c7460ff70a6280056c2f7",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "bcbfc97258667f152733",
+    "url": "/todo/static/css/main.dad4c9e5.chunk.css"
+  },
+  {
+    "revision": "2b7102446546307994dc",
+    "url": "/todo/static/js/2.c881ae52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.c881ae52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcbfc97258667f152733",
+    "url": "/todo/static/js/main.6fc1498e.chunk.js"
+  },
+  {
+    "revision": "da7c3f7f7e5963fe74b8",
+    "url": "/todo/static/js/runtime-main.74315f96.js"
+  }
+]);
